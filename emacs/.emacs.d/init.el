e6700959f5e86b8efcec6071dd37c4add11efa39
CoolMannix <xiaomc95@gmail.com>
2020-09-18 10:26:53 +0800

    update

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,13 @@
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
+(package-initialize)
+
+(global-linum-mode 1)
+
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-init-file)
